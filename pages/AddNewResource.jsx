daeba5f087143a761ea865e44b5dc9bb418e8ead
GrowYourOwnFreedom <bigbelchis@hotmail.com>
2023-06-20 09:26:00 +0100

    util building

--- pages/AddNewResource.jsx
+++ pages/AddNewResource.jsx
@@ -8,157 +8,167 @@ import { UserContext } from "../contexts/UserContext";
 import BackButton from "../components/BackButton";
 
 export const AddNewResource = ({ route }) => {
-  const [submitError, setSubmitError] = useState(null);
+	const [submitError, setSubmitError] = useState(null);
 
-  const abundanceValues = ["depleted", "low", "medium", "high", "abundant"];
-  const { image, location } = route.params;
+	const abundanceValues = ["depleted", "low", "medium", "high", "abundant"];
+	const { image, location } = route.params;
 
-  const { user: username } = useContext(UserContext);
+	const { user: username } = useContext(UserContext);
 
-  const handleSubmit = ({
-    resource_name,
-    description,
-    condition,
-    depletion,
-  }) => {
-    const formData = new FormData();
-    formData.append("image", image);
-    formData.append("resource_name", resource_name);
-    formData.append("description", description);
-    formData.append("condition", condition);
-    formData.append("depletion", depletion);
-    formData.append("username", username);
-    formData.append("latitude", location.coords.latitude);
-    formData.append("longitude", location.coords.longitude);
-    formData.append("created_at", new Date(location.timestamp).toUTCString());
+	const handleSubmit = ({
+		resource_name,
+		description,
+		condition,
+		depletion,
+	}) => {
+		console.log(image.uri);
+		const formData = new FormData();
+		formData.append("image", {
+			uri: image.uri,
+			type: 'image/jpeg',
+			name: "appImage.jpeg",
+		});
+		formData.append("resource_name", resource_name);
+		formData.append("description", description);
+		formData.append("condition", condition);
+		formData.append("depletion", depletion);
+		formData.append("username", username);
+		formData.append("latitude", location.coords.latitude);
+		formData.append("longitude", location.coords.longitude);
+		formData.append(
+			"created_at",
+			new Date(location.timestamp).toUTCString()
+		);
 
-    // formData.append("resource", body)
+		postResource(formData).catch(() => {
+			setSubmitError(
+				"Sorry resource was not submitted, please restart the app and try again!"
+			);
+		});
+	};
 
-    postResource(formData).catch(() => {
-      setSubmitError(
-        "Sorry resource was not submitted, please restart the app and try again!"
-      );
-    });
-  };
-
-  return (
-    <View style={styles.container}>
-      <BackButton />
-      <View style={styles.title}>
-        <Text>Add new resource</Text>
-      </View>
-      <View style={styles.imageContainer}>
-        <Image source={{ uri: image.uri }} style={styles.image} />
-      </View>
-      <View style={styles.formContainer}>
-        <Formik
-          initialValues={{
-            resource_name: "",
-            description: "",
-            condition: 50,
-            depletion: 3,
-          }}
-          onSubmit={handleSubmit}
-        >
-          {({
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            values,
-            setFieldValue,
-          }) => (
-            <>
-              <Text>Name of Forageable resource</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Name"
-                onChangeText={handleChange("resource_name")}
-                value={values.resource_name}
-              />
-              <Text>Notes about Forageable resource?</Text>
-              <TextInput
-                style={styles.notes_input}
-                placeholder="Notes"
-                onChangeText={handleChange("description")}
-                value={values.description}
-              />
-              <Text>Quality: {values.condition}%</Text>
-              <Slider
-                style={styles.slider}
-                value={values.condition}
-                minimumValue={0}
-                maximumValue={100}
-                onValueChange={(value) => {
-                  setFieldValue("condition", value);
-                }}
-                step={1}
-              />
-              <Text>Abundance: {abundanceValues[values.depletion]}</Text>
-              <Slider
-                style={styles.slider}
-                value={values.depletion}
-                minimumValue={0}
-                maximumValue={4}
-                onValueChange={(value) => {
-                  setFieldValue("depletion", value);
-                }}
-                step={1}
-              />
-              {submitError && <Text style={styles.error}>{submitError}</Text>}
-              <Button onPress={handleSubmit} title="Submit" />
-            </>
-          )}
-        </Formik>
-      </View>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<BackButton />
+			<View style={styles.title}>
+				<Text>Add new resource</Text>
+			</View>
+			<View style={styles.imageContainer}>
+				<Image source={{ uri: image.uri }} style={styles.image} />
+			</View>
+			<View style={styles.formContainer}>
+				<Formik
+					initialValues={{
+						resource_name: "",
+						description: "",
+						condition: 50,
+						depletion: 3,
+					}}
+					onSubmit={handleSubmit}
+				>
+					{({
+						handleChange,
+						handleBlur,
+						handleSubmit,
+						values,
+						setFieldValue,
+					}) => (
+						<>
+							<Text>Name of Forageable resource</Text>
+							<TextInput
+								style={styles.input}
+								placeholder="Name"
+								onChangeText={handleChange("resource_name")}
+								value={values.resource_name}
+							/>
+							<Text>Notes about Forageable resource?</Text>
+							<TextInput
+								style={styles.notes_input}
+								placeholder="Notes"
+								onChangeText={handleChange("description")}
+								value={values.description}
+							/>
+							<Text>Quality: {values.condition}%</Text>
+							<Slider
+								style={styles.slider}
+								value={values.condition}
+								minimumValue={0}
+								maximumValue={100}
+								onValueChange={(value) => {
+									setFieldValue("condition", value);
+								}}
+								step={1}
+							/>
+							<Text>
+								Abundance: {abundanceValues[values.depletion]}
+							</Text>
+							<Slider
+								style={styles.slider}
+								value={values.depletion}
+								minimumValue={0}
+								maximumValue={4}
+								onValueChange={(value) => {
+									setFieldValue("depletion", value);
+								}}
+								step={1}
+							/>
+							{submitError && (
+								<Text style={styles.error}>{submitError}</Text>
+							)}
+							<Button onPress={handleSubmit} title="Submit" />
+						</>
+					)}
+				</Formik>
+			</View>
+		</View>
+	);
 };
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 16,
-  },
-  input: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    marginBottom: 16,
-    paddingHorizontal: 8,
-  },
-  notes_input: {
-    height: 200,
-    borderColor: "gray",
-    borderWidth: 1,
-    marginBottom: 16,
-    paddingHorizontal: 8,
-  },
-  title: {
-    alignItems: "center",
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "center",
-    marginTop: 30,
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 16,
-  },
-  formContainer: {
-    flex: 1,
-  },
-  imageContainer: {
-    margin: 30,
-    height: "30%",
-    backgroundColor: "#fff",
-  },
-  slider: { width: 200, height: 40 },
-  image: {
-    flex: 1,
-    width: undefined,
-    height: undefined,
-    backgroundColor: "#0553",
-    contentFit: "contain",
-  },
-  error: {
-    color: "#f44336",
-  },
+	container: {
+		flex: 1,
+		paddingHorizontal: 16,
+	},
+	input: {
+		height: 40,
+		borderColor: "gray",
+		borderWidth: 1,
+		marginBottom: 16,
+		paddingHorizontal: 8,
+	},
+	notes_input: {
+		height: 200,
+		borderColor: "gray",
+		borderWidth: 1,
+		marginBottom: 16,
+		paddingHorizontal: 8,
+	},
+	title: {
+		alignItems: "center",
+		flexDirection: "row",
+		width: "100%",
+		justifyContent: "center",
+		marginTop: 30,
+		fontSize: 20,
+		fontWeight: "bold",
+		marginBottom: 16,
+	},
+	formContainer: {
+		flex: 1,
+	},
+	imageContainer: {
+		margin: 30,
+		height: "30%",
+		backgroundColor: "#fff",
+	},
+	slider: { width: 200, height: 40 },
+	image: {
+		flex: 1,
+		width: undefined,
+		height: undefined,
+		backgroundColor: "#0553",
+		contentFit: "contain",
+	},
+	error: {
+		color: "#f44336",
+	},
 });
--- utils/utils.js
+++ utils/utils.js
@@ -1,267 +1,276 @@
 import axios from "axios";
 const foragingApi = axios.create({
-	baseURL: "https://vgk9m0b765.execute-api.eu-west-1.amazonaws.com",
+	baseURL: "https://u08cns9a09.execute-api.us-east-1.amazonaws.com",
 });
 
 export const fetchItems = (params) => {
-  let items = [];
-  if (!params) {
-    items = [
-      {
-        resource_id: "1",
-        resource_name: "black raspberry",
-        location: {
-          latitude: 50.82998419421786,
-          longitude: -0.15759806528483825,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/06/2023",
-        description: "a lovely bunch of raspberries",
-        img_url:
-          "https://images.pexels.com/photos/479454/pexels-photo-479454.jpeg",
-      },
-      {
-        resource_id: "2",
-        resource_name: "blueberries",
-        location: {
-          latitude: 50.83542426202701,
-          longitude: -0.1550466660217834,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/05/2023",
-        description: "a lovely bunch of blueberries",
-      },
-      {
-        resource_id: "3",
-        resource_name: "blackberries",
-        location: {
-          latitude: 50.83963065065905,
-          longitude: -0.16770397845957438,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/04/2023",
-        description: "a lovely bunch of blueberries",
-      },
-      {
-        resource_id: "4",
-        resource_name: "redcurrants",
-        location: {
-          latitude: 50.84281606610109,
-          longitude: -0.17208415690061635,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/03/2023",
-        description: "a lovely bunch of blueberries",
-      },
-      {
-        resource_id: "5",
-        resource_name: "gooseberries",
-        location: {
-          latitude: 50.8433276085873,
-          longitude: -0.17266901220018463,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/02/2023",
-        description: "a lovely bunch of blueberries",
-      },
-    ];
-  } else if (params.params.search.includes("berries")) {
-    items = [
-      {
-        resource_id: "2",
-        resource_name: "blueberries",
-        location: {
-          latitude: 50.83542426202701,
-          longitude: -0.1550466660217834,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/05/2023",
-        description: "a lovely bunch of blueberries",
-      },
-      {
-        resource_id: "3",
-        resource_name: "blackberries",
-        location: {
-          latitude: 50.83963065065905,
-          longitude: -0.16770397845957438,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/04/2023",
-        description: "a lovely bunch of blueberries",
-      },
-      {
-        resource_id: "5",
-        resource_name: "gooseberries",
-        location: {
-          latitude: 50.8433276085873,
-          longitude: -0.17266901220018463,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/02/2023",
-        description: "a lovely bunch of blueberries",
-      },
-    ];
-    console.log(params.params.search.includes("berries"));
-  } else {
-    items = [
-      {
-        resource_id: "1",
-        resource_name: "black raspberry",
-        location: {
-          latitude: 50.82998419421786,
-          longitude: -0.15759806528483825,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/06/2023",
-        description: "a lovely bunch of raspberries",
-        img_url:
-          "https://images.pexels.com/photos/479454/pexels-photo-479454.jpeg",
-      },
-      {
-        resource_id: "2",
-        resource_name: "blueberries",
-        location: {
-          latitude: 50.83542426202701,
-          longitude: -0.1550466660217834,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/05/2023",
-        description: "a lovely bunch of blueberries",
-      },
-      {
-        resource_id: "3",
-        resource_name: "blackberries",
-        location: {
-          latitude: 50.83963065065905,
-          longitude: -0.16770397845957438,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/04/2023",
-        description: "a lovely bunch of blueberries",
-      },
-      {
-        resource_id: "4",
-        resource_name: "redcurrants",
-        location: {
-          latitude: 50.84281606610109,
-          longitude: -0.17208415690061635,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/03/2023",
-        description: "a lovely bunch of blueberries",
-      },
-      {
-        resource_id: "5",
-        resource_name: "gooseberries",
-        location: {
-          latitude: 50.8433276085873,
-          longitude: -0.17266901220018463,
-        },
-        condition: 90,
-        depletion: 10,
-        username: "Chris",
-        created_at: "12/02/2023",
-        description: "a lovely bunch of blueberries",
-      },
-    ];
-  }
+	let items = [];
+	if (!params) {
+		items = [
+			{
+				resource_id: "1",
+				resource_name: "black raspberry",
+				location: {
+					latitude: 50.82998419421786,
+					longitude: -0.15759806528483825,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/06/2023",
+				description: "a lovely bunch of raspberries",
+				img_url:
+					"https://images.pexels.com/photos/479454/pexels-photo-479454.jpeg",
+			},
+			{
+				resource_id: "2",
+				resource_name: "blueberries",
+				location: {
+					latitude: 50.83542426202701,
+					longitude: -0.1550466660217834,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/05/2023",
+				description: "a lovely bunch of blueberries",
+			},
+			{
+				resource_id: "3",
+				resource_name: "blackberries",
+				location: {
+					latitude: 50.83963065065905,
+					longitude: -0.16770397845957438,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/04/2023",
+				description: "a lovely bunch of blueberries",
+			},
+			{
+				resource_id: "4",
+				resource_name: "redcurrants",
+				location: {
+					latitude: 50.84281606610109,
+					longitude: -0.17208415690061635,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/03/2023",
+				description: "a lovely bunch of blueberries",
+			},
+			{
+				resource_id: "5",
+				resource_name: "gooseberries",
+				location: {
+					latitude: 50.8433276085873,
+					longitude: -0.17266901220018463,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/02/2023",
+				description: "a lovely bunch of blueberries",
+			},
+		];
+	} else if (params.params.search.includes("berries")) {
+		items = [
+			{
+				resource_id: "2",
+				resource_name: "blueberries",
+				location: {
+					latitude: 50.83542426202701,
+					longitude: -0.1550466660217834,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/05/2023",
+				description: "a lovely bunch of blueberries",
+			},
+			{
+				resource_id: "3",
+				resource_name: "blackberries",
+				location: {
+					latitude: 50.83963065065905,
+					longitude: -0.16770397845957438,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/04/2023",
+				description: "a lovely bunch of blueberries",
+			},
+			{
+				resource_id: "5",
+				resource_name: "gooseberries",
+				location: {
+					latitude: 50.8433276085873,
+					longitude: -0.17266901220018463,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/02/2023",
+				description: "a lovely bunch of blueberries",
+			},
+		];
+		console.log(params.params.search.includes("berries"));
+	} else {
+		items = [
+			{
+				resource_id: "1",
+				resource_name: "black raspberry",
+				location: {
+					latitude: 50.82998419421786,
+					longitude: -0.15759806528483825,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/06/2023",
+				description: "a lovely bunch of raspberries",
+				img_url:
+					"https://images.pexels.com/photos/479454/pexels-photo-479454.jpeg",
+			},
+			{
+				resource_id: "2",
+				resource_name: "blueberries",
+				location: {
+					latitude: 50.83542426202701,
+					longitude: -0.1550466660217834,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/05/2023",
+				description: "a lovely bunch of blueberries",
+			},
+			{
+				resource_id: "3",
+				resource_name: "blackberries",
+				location: {
+					latitude: 50.83963065065905,
+					longitude: -0.16770397845957438,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/04/2023",
+				description: "a lovely bunch of blueberries",
+			},
+			{
+				resource_id: "4",
+				resource_name: "redcurrants",
+				location: {
+					latitude: 50.84281606610109,
+					longitude: -0.17208415690061635,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/03/2023",
+				description: "a lovely bunch of blueberries",
+			},
+			{
+				resource_id: "5",
+				resource_name: "gooseberries",
+				location: {
+					latitude: 50.8433276085873,
+					longitude: -0.17266901220018463,
+				},
+				condition: 90,
+				depletion: 10,
+				username: "Chris",
+				created_at: "12/02/2023",
+				description: "a lovely bunch of blueberries",
+			},
+		];
+	}
 
-  return Promise.resolve(items);
+	return Promise.resolve(items);
 };
 
 export const fetchCommentsByResourceId = (resource_id) => {
-  const comments = [
-    {
-      resource_id: 1,
-      comment_id: 1,
-      username: "James",
-      created_at: "13/06/2023",
-      body: "comment 1",
-    },
-    {
-      resource_id: 1,
-      comment_id: 2,
-      username: "Andrii",
-      created_at: "13/05/2023",
-      body: "comment 2",
-    },
-    {
-      resource_id: 2,
-      comment_id: 1,
-      username: "Jack",
-      created_at: "13/04/2023",
-      body: "comment 3",
-    },
-    {
-      resource_id: 2,
-      comment_id: 2,
-      username: "Sarah",
-      created_at: "13/03/2023",
-      body: "comment 4",
-    },
-    {
-      resource_id: 3,
-      comment_id: 1,
-      username: "Chris",
-      created_at: "13/02/2023",
-      body: "comment 5",
-    },
-    {
-      resource_id: 4,
-      comment_id: 1,
-      username: "Conor",
-      created_at: "13/01/2023",
-      body: "comment 6",
-    },
-    {
-      resource_id: 4,
-      comment_id: 2,
-      username: "James",
-      created_at: "13/12/2022",
-      body: "comment 7",
-    },
-  ];
+	const comments = [
+		{
+			resource_id: 1,
+			comment_id: 1,
+			username: "James",
+			created_at: "13/06/2023",
+			body: "comment 1",
+		},
+		{
+			resource_id: 1,
+			comment_id: 2,
+			username: "Andrii",
+			created_at: "13/05/2023",
+			body: "comment 2",
+		},
+		{
+			resource_id: 2,
+			comment_id: 1,
+			username: "Jack",
+			created_at: "13/04/2023",
+			body: "comment 3",
+		},
+		{
+			resource_id: 2,
+			comment_id: 2,
+			username: "Sarah",
+			created_at: "13/03/2023",
+			body: "comment 4",
+		},
+		{
+			resource_id: 3,
+			comment_id: 1,
+			username: "Chris",
+			created_at: "13/02/2023",
+			body: "comment 5",
+		},
+		{
+			resource_id: 4,
+			comment_id: 1,
+			username: "Conor",
+			created_at: "13/01/2023",
+			body: "comment 6",
+		},
+		{
+			resource_id: 4,
+			comment_id: 2,
+			username: "James",
+			created_at: "13/12/2022",
+			body: "comment 7",
+		},
+	];
 
-  return Promise.resolve(comments);
+	return Promise.resolve(comments);
 };
 
 export const postComment = (resource_id, body, username) => {
-  const newComment = {
-    resource_id,
-    body,
-    username,
-  };
+	const newComment = {
+		resource_id,
+		body,
+		username,
+	};
 
-  console.log(newComment);
+	console.log(newComment);
 };
 
 export const postResource = (formData) => {
-  console.log(formData);
-
- return foragingApi.post("/api/resources", formData, { headers: {'Content-Type': 'multipart/form-data'}}).catch(error=> console.log(error))
-// return foragingApi.post("/api/resources").then((data)=>{console.log(data.data.spots.Items)}).catch(error=> console.log(error))
-}
+	console.log("request made");
+	// console.log(formData, "<<<<<<<<< form data");
+	return foragingApi
+		.post("/api/resources", formData, {
+			headers: { "Content-Type": "multipart/form-data" },
+		})
+		.then((response) =>
+			console.log(response.data, "<<<<<<<<<<<<<<<server response")
+		)
+		.catch((error) =>
+			console.log(error, "<<<<<<<<<<<<<<<<<<<server error")
+		);
+	
+};
